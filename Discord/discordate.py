62cf7e4b195652d74bcde63f4fe8b34480632549
Yan Osher Cohen <74152828+yanoshercohen@users.noreply.github.com>
2021-09-28 09:00:30 -0700

    Update discordate.py

--- Discord/discordate.py
+++ Discord/discordate.py
@@ -13,8 +13,8 @@ def main():
 def convert_id_to_date(user_id):
     timestamp = ((int(user_id) >> 22) + 1420070400000)//1000
     if not 0 <= timestamp <= 32536799999:
-        raise ValueError(f"Discord ID \"{user_id}\" is not valid.")
-    return f"The creation date of \"{user_id}\" is {datetime.fromtimestamp(timestamp)}"
+        raise ValueError(f'Discord ID "{user_id}" is not valid.')
+    return f'The creation date of "{user_id}" is {datetime.fromtimestamp(timestamp)}'
     
 if __name__ == "__main__":
     main()
